e4d57295eea36271501194bbc3ae216604c7043a
K-nass <karimnase10@gmail.com>
2025-06-08 01:10:35 +0300

    added accordion to experance section

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { ExperienceItemComponent } from "./experience-item/experience-item.component";
+import { HandleExperienceService } from '../services/handle-experience.service';
+import { ExperienceInterface } from '../interfaces/experanceinterface';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [ExperienceItemComponent],
+  imports: [ExperienceItemComponent, CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
- 
+  experienceData: ExperienceInterface = {
+    companyName: '',
+    role: '',
+    startYear: '',
+    endYear: '',
+    description: '',
+    presentState: false
+  }
+  constructor(private handleExperienceService: HandleExperienceService) { }
 }
